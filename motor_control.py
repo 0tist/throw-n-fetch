8c3a64c89d56791833a7252812c7b391ff84b840
Tomi Lahti <tomi.lahti2@gmail.com>
2024-03-17 02:46:07 +0200

    Update motor control code to use RPi.GPIO library

--- motor_control.py
+++ motor_control.py
@@ -1,12 +1,26 @@
-from gpiozero import Servo
-from time import sleep
+import time
 
-servo = Servo(23)  # try 23
+import RPi.GPIO as GPIO
 
-while True:
-    servo.min()  # Move servo to minimum position
-    sleep(1)
-    servo.mid()  # Move servo to middle position
-    sleep(1)
-    servo.max()  # Move servo to maximum position
-    sleep(1)
+servoPIN = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+pwm = GPIO.PWM(servoPIN, 50)  # GPIO 4 for PWM with 50Hz
+pwm.start(0)  # Initialization
+
+try:
+    while True:
+        print("Turning towards 0 degree")
+        pwm.ChangeDutyCycle(2.5)  # Turn towards 0 degree
+        time.sleep(0.5)
+        print("Turning towards 90 degree")
+        pwm.ChangeDutyCycle(7.5)  # Turn towards 90 degree
+        time.sleep(0.5)
+        print("Turning towards 180 degree")
+        pwm.ChangeDutyCycle(12.5)  # Turn towards 180 degree
+        time.sleep(0.5)
+finally:
+    print("Stopping the motor")
+    pwm.stop()
+    GPIO.cleanup()
