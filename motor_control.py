5f71a0261df8a6cbd2fa68f84ab13a6f9fdc2254
Tomi Lahti <tomi.lahti2@gmail.com>
2024-03-17 02:45:13 +0200

    Update motor control code to use gpiozero library

--- motor_control.py
+++ motor_control.py
@@ -1,21 +1,12 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Servo
+from time import sleep
 
-servoPIN = 5
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+servo = Servo(23)  # try 23
 
-pwm = GPIO.PWM(servoPIN, 50)  # GPIO 4 for PWM with 50Hz
-pwm.start(0)  # Initialization
-
-try:
-    while True:
-        pwm.ChangeDutyCycle(2.5)  # Turn towards 0 degree
-        time.sleep(0.5)
-        pwm.ChangeDutyCycle(7.5)  # Turn towards 90 degree
-        time.sleep(0.5)
-        pwm.ChangeDutyCycle(12.5)  # Turn towards 180 degree
-        time.sleep(0.5)
-finally:
-    pwm.stop()
-    GPIO.cleanup()
+while True:
+    servo.min()  # Move servo to minimum position
+    sleep(1)
+    servo.mid()  # Move servo to middle position
+    sleep(1)
+    servo.max()  # Move servo to maximum position
+    sleep(1)
