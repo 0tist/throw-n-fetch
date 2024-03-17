08d007960f69f4b61ba08d12a27c27b3c4e8f1ce
Tomi Lahti <tomi.lahti2@gmail.com>
2024-03-17 02:12:46 +0200

    Update motor control code to use RPi.GPIO library

--- motor_control.py
+++ motor_control.py
@@ -1,17 +1,21 @@
-import pigpio
+import RPi.GPIO as GPIO
 import time
 
 servoPIN = 16
-pi = pigpio.pi()  # Connect to local Pi.
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+pwm = GPIO.PWM(servoPIN, 50)  # GPIO 4 for PWM with 50Hz
+pwm.start(0)  # Initialization
 
 try:
     while True:
-        pi.set_servo_pulsewidth(servoPIN, 500)  # Small pulse for one direction
+        pwm.ChangeDutyCycle(2.5)  # Turn towards 0 degree
         time.sleep(0.5)
-        pi.set_servo_pulsewidth(servoPIN, 1500)  # Medium pulse for neutral position
+        pwm.ChangeDutyCycle(7.5)  # Turn towards 90 degree
         time.sleep(0.5)
-        pi.set_servo_pulsewidth(servoPIN, 2500)  # Large pulse for the other direction
+        pwm.ChangeDutyCycle(12.5)  # Turn towards 180 degree
         time.sleep(0.5)
 finally:
-    pi.set_servo_pulsewidth(servoPIN, 0)  # Turn off pulse
-    pi.stop()
+    pwm.stop()
+    GPIO.cleanup()
