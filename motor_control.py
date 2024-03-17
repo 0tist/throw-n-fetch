ea6dccfb1aebc0fc496b11bce1c78203a2351862
Tomi Lahti <tomi.lahti2@gmail.com>
2024-03-17 02:14:29 +0200

    Update servoPIN value and add print statements for motor control

--- motor_control.py
+++ motor_control.py
@@ -1,7 +1,8 @@
-import RPi.GPIO as GPIO
 import time
 
-servoPIN = 16
+import RPi.GPIO as GPIO
+
+servoPIN = 5
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
@@ -10,12 +11,16 @@ pwm.start(0)  # Initialization
 
 try:
     while True:
+        print("Turning towards 0 degree")
         pwm.ChangeDutyCycle(2.5)  # Turn towards 0 degree
         time.sleep(0.5)
+        print("Turning towards 90 degree")
         pwm.ChangeDutyCycle(7.5)  # Turn towards 90 degree
         time.sleep(0.5)
+        print("Turning towards 180 degree")
         pwm.ChangeDutyCycle(12.5)  # Turn towards 180 degree
         time.sleep(0.5)
 finally:
+    print("Stopping the motor")
     pwm.stop()
     GPIO.cleanup()
